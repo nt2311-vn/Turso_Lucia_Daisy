9079e68c13bda3f59522f5c47d8fe2c61aade6dd
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-28 11:21:57 +0700

    delete all project and start new ones

--- src/lib/server/db/index.js
+++
@@ -1,8 +0,0 @@
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
-import { env } from "$env/dynamic/private";
-
-export const libsqlClient = createClient({
-	url: /** @type {string} */ (env.TURSO_CONNECTION_URL),
-	authToken: /** @type {string} */ (env.TURSO_AUTH_TOKEN),
-});
